8703f060e3eeb72ec7e17aebaecc044bd5bd604f
Luke <lpose22@gmail.com>
2020-12-16 01:09:26 -0600

    add reqs and defi pulse example

---
+++ defi_pulse.py
@@ -0,0 +1,11 @@
+import requests 
+import json 
+
+
+# input your token here
+api_key = ''
+
+url = 'https://data-api.defipulse.com/api/v1/defipulse/api/GetHistory?api-key=' + api_key
+response = requests.get(url)
+result = json.loads(response.text)
+print(result[0])
